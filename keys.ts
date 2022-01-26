51870cf975f9c46a09a0a636ec19f8a1130b9e9b
Ivan Martinez <50034274+IvanMartinezz@users.noreply.github.com>
2022-01-26 18:17:32 -0300

    Update keys.ts

--- keys.ts
+++ keys.ts
@@ -1,20 +1,5 @@
 export default {
 
-    database: {
-        connectionLimit: 10,
-        host: 'database-1.ch0pvhbbxkuy.sa-east-1.rds.amazonaws.com',
-        user: 'root',
-        password: 'gimnasio123',
-        database: 'gym'
-    },
-    database1: {
-        host : 'database-1.ch0pvhbbxkuy.sa-east-1.rds.amazonaws.com',
-        user : 'root',
-        password : 'gimnasio123',
-        database : 'gym',
-        dialect : 'mysql'
-    },
-
     test: {
         host: 'localhost',
         user: 'root',
